ed9207393033acb9744417b4e68cf9b8e8b1834b
Anton Bukov <k06aaa@gmail.com>
2015-11-25 22:29:56 +0300

    0.0.3

--- UINavigationController+StatusBar.m
+++ UINavigationController+StatusBar.m
@@ -5,6 +5,7 @@
 //  Copyright (c) 2015 Anton Bukov. All rights reserved.
 //
 
+#import <JRSwizzle/JRSwizzle.h>
 #import "UINavigationController+StatusBar.h"
 
 @implementation UINavigationController (StatusBar)
@@ -17,6 +18,22 @@
 - (UIViewController *)childViewControllerForStatusBarHidden
 {
     return self.visibleViewController;
+}
+
+- (BOOL)xxx_shouldAutorotate
+{
+    return self.visibleViewController ? [self.visibleViewController shouldAutorotate] : YES;
+}
+
+- (UIInterfaceOrientationMask)xxx_supportedInterfaceOrientations
+{
+    return [self.visibleViewController supportedInterfaceOrientations];
+}
+
++ (void)load
+{
+    [self jr_swizzleMethod:@selector(shouldAutorotate) withMethod:@selector(xxx_shouldAutorotate) error:NULL];
+    [self jr_swizzleMethod:@selector(supportedInterfaceOrientations) withMethod:@selector(xxx_supportedInterfaceOrientations) error:NULL];
 }
 
 @end
